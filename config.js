160374471325938b165e86b2cec0acc891e6e8b6
Aaron Ogle <atogle@gmail.com>
2013-06-01 22:00:10 -0400

    Add healthy stores

--- config.js
+++ config.js
@@ -6,10 +6,17 @@ Leopold.Config = [
     layers: [
       {
         url: 'http://gis.phila.gov/ArcGIS/rest/services/PhilaGov/Farmers_Markets/MapServer/0/query',
-        template: '<div class="span6">{{NAME}}</div>',
+        template: '<div class="span6">{{NAME}} (farmers market)</div>',
+        buffer: 1.0, //in miles
+        agsOptions: {} //just use defaults
+      },
+      {
+        url: 'http://gis.phila.gov/ArcGIS/rest/services/PhilaGov/Healthy_Corner_Stores/MapServer/0/query',
+        template: '<div class="span6">{{OFFICIAL_S}} (healthy store)</div>',
         buffer: 1.0, //in miles
         agsOptions: {} //just use defaults
       }
+
     ]
   }
 ];
